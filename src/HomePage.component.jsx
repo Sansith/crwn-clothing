2d8ebfd46974bef32b28fe4663bd743644b99dd6
seniyas <seniyadissanayake@gmail.com>
2020-10-03 07:23:56 +0530

    intial commit

---
+++ src/HomePage.component.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hello</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">T-Shirts</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hello</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hello</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
